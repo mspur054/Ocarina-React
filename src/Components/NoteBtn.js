01be22848977ed70554c1f2284d63227ab06364f
mspur054 <mspur054@uottawa.ca>
2019-03-20 19:06:00 -0400

    converted NoteBtn to functional and destructured

--- src/Components/NoteBtn.js
+++ src/Components/NoteBtn.js
@@ -1,33 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-class NoteBtn extends React.Component {
+const NoteBtn = ({ dataKey, index, keyPosition }) => {
   //TODO: convert to function component
-  static propTypes = {
+  NoteBtn.propTypes = {
     "data-key": PropTypes.number
   };
+  let className = `noteBtn${
+    //if the key has been played make it solid
+    keyPosition > index ? " maxOpacity" : ""
+  }`;
 
-  render() {
-    let className = `noteBtn${
-      //if the key has been played make it solid
-      this.props.keyPosition > this.props.index ? " maxOpacity" : ""
-    }`;
-
-    //Pass in keypad icon
-    if (this.props.dataKey === 37) {
-      className += " noteBtn-left";
-    } else if (this.props.dataKey === 38) {
-      className += " noteBtn-up";
-    } else if (this.props.dataKey === 39) {
-      className += " noteBtn-right";
-    } else if (this.props.dataKey === 40) {
-      className += " noteBtn-down";
-    } else if (this.props.dataKey === 65) {
-      className += " noteBtn-aKey";
-    }
-
-    return <button data-key={this.props.dataKey} className={className} />;
+  //Pass in keypad icon
+  if (dataKey === 37) {
+    className += " noteBtn-left";
+  } else if (dataKey === 38) {
+    className += " noteBtn-up";
+  } else if (dataKey === 39) {
+    className += " noteBtn-right";
+  } else if (dataKey === 40) {
+    className += " noteBtn-down";
+  } else if (dataKey === 65) {
+    className += " noteBtn-aKey";
   }
-}
+  return <button data-key={dataKey} className={className} />;
+};
 
 export default NoteBtn;
